6f03caf264536a02f3e2daab1b77770bf1c78924
João <jpauloxpo@gmail.com>
2024-02-24 20:41:38 -0300

    Adicionando a classe tipos de variaveis

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,3 +1,17 @@
+import java.util.Locale;
+
 public class TiposVariaveis {
+
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int idade; //tipo "int", nome " idade", com nenhum valor atribuído.
+        int anoFabricacao = 2021; //tipo "int", nome "anoFabricação" valor atribuído "2021"'.
+        double salarioMinimo = 2500.33;
+
+    }
+
 
 }
